2a4b84aee3ba52e564485b6ed479de0a47a8a04e
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-24 14:27:54 -0300

    Added: .env _redirects component search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css"
-
+import Search from "./components/search"
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+     <Search/>
     </div>
   )
 }
