b9b9f853daa10ba58aba7bd80afea30fda7d908c
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-24 20:41:10 -0300

    Added: Footer

---
+++ src/components/Endbar.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+function Endbar() {
+  return (
+    <div className="fixed bottom-0 w-full p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 bg-blue-500">
+      <span className="text-sm font-bold text-white sm:text-center ">
+        © 2022
+        <a href="" className="hover:underline">
+          Mnslvs™
+        </a>
+        . All Rights Reserved.
+      </span>
+      <ul className="flex flex-wrap items-center mt-3 text-sm font-bold text-white  sm:mt-0">
+        <li>
+          <a href="" className="mr-4 hover:underline md:mr-6 ">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="" className="mr-4 hover:underline md:mr-6">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="" className="mr-4 hover:underline md:mr-6">
+            Licensing
+          </a>
+        </li>
+        <li>
+          <a href="" className="hover:underline">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Endbar
