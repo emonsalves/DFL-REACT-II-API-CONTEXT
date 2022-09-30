b6b2b92eb889cf413485532f9fcf27628f82cfb8
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-30 17:21:01 -0300

    Fix : href

--- src/components/Endbar.jsx
+++ src/components/Endbar.jsx
@@ -5,29 +5,29 @@ function Endbar() {
     <div className="pl-10 p-4 justify-center shadow md:flex md:items-center md:justify-between md:p-6 bg-blue-500">
       <span className="text-sm font-bold text-white sm:text-center ">
         © 2022
-        <a href="" className="hover:underline">
+        <a className="hover:underline">
           Mnslvs™
         </a>
         . All Rights Reserved.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm font-bold text-white sm:mt-0">
         <li>
-          <a href="" className="mr-4 hover:underline md:mr-6 ">
+          <a className="mr-4 hover:underline md:mr-6 ">
             About
           </a>
         </li>
         <li>
-          <a href="" className="mr-4 hover:underline md:mr-6">
+          <a className="mr-4 hover:underline md:mr-6">
             Privacy Policy
           </a>
         </li>
         <li>
-          <a href="" className="mr-4 hover:underline md:mr-6">
+          <a className="mr-4 hover:underline md:mr-6">
             Licensing
           </a>
         </li>
         <li>
-          <a href="" className="hover:underline">
+          <a className="hover:underline">
             Contact
           </a>
         </li>
