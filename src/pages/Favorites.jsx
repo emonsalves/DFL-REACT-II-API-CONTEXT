83a0071c096629e4bb2cfceb344261882fa97607
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-30 17:12:25 -0300

    Fix : change to new array map

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,14 +1,14 @@
-import React, { useContext } from "react"
+import { useContext } from "react"
 import MyContext from "../context/MyContext"
 import Card from "../components/Card/Card"
 
 function Favorites() {
-  const { data, setData } = useContext(MyContext)
+  const { array } = useContext(MyContext)
 
   return (
     <div className="Container flex flex-wrap gap-8 justify-center my-4">
-      {data &&
-        data.map(
+      {array &&
+        array.map(
           (dato, id) =>
             dato.like === true && (
               <Card
