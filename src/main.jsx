c5da166c3956cdbce37eef5f52b5b2559cacd19a
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-30 22:09:20 -0300

    Feat : Interceptors Axios

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import "./index.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import { BrowserRouter } from "react-router-dom"
 import MyContext from "./context/MyProvider"
+import axios from "axios"
+
+axios.interceptors.request.use((request) => {
+  return request
+})
+
+axios.interceptors.response.use((response) => {
+  return response
+})
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MyContext>
